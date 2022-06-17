6972a3efed49e106613f3124d6c821091e39e19c
Foreinyel <Foreinyel@163.com>
2022-06-17 15:28:09 +0800

    react-component-library-bridge-template

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef, useLayoutEffect, useState } from "react";
+const useCustomLayoutEffect =
+  typeof window !== "undefined" ? useLayoutEffect : useEffect;
+const load = System.import("{{ resourceUrl }}");
+
+
+{% for componentName in componentNames %}
+  export const {{ componentName }} = (props) => {
+  const [loaded, setLoaded] = useState(false);
+  const ref = useRef();
+
+  useCustomLayoutEffect(() => {
+    load.then((mod) => {
+      ref.current = mod.{{ componentName }};
+      setLoaded(true);
+    });
+  }, []);
+
+  const Component = ref.current as unknown as React.FC;
+
+  return loaded && Component ? <Component {...props} /> : null;
+};
+{% else %}
+  export {}
+{% endfor %}
